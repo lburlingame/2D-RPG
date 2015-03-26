d8752d578de26f4ce73d9b394bfd9aebfd5059d8
lburlingame <lelandburlingame@gmail.com>
2015-03-26 10:29:15 -0700

    testing commit to branch

---
+++ Dreamstreet/src/com/dreamstreet/arpg/item/Item.java
@@ -0,0 +1,7 @@
+package com.dreamstreet.arpg.item;
+
+/**
+ * Created on 3/26/2015.
+ */
+public class Item {
+}
---
+++ Dreamstreet/src/com/dreamstreet/arpg/sfx/MusicPlayer.java
@@ -0,0 +1,7 @@
+package com.dreamstreet.arpg.sfx;
+
+/**
+ * Created on 3/26/2015.
+ */
+public class MusicPlayer {
+}
