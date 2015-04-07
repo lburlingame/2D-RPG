e83446d1636793d1b11115cc468ab856ac891952
lburlingame <lelandburlingame@gmail.com>
2015-04-06 23:23:24 -0700

    updated
    fixed workspace.xml error

---
+++ Dreamstreet/src/com/dreamstreet/arpg/gfx/Vector2.java
@@ -0,0 +1,15 @@
+package com.dreamstreet.arpg.gfx;
+
+/**
+ * Created on 4/6/2015.
+ */
+public class Vector2 {
+
+    double x;
+    double y;
+
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
