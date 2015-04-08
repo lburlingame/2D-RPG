079b30e89d6beebb8b3adf91b0ccd388a8f9052c
lburlingame <lelandburlingame@gmail.com>
2015-04-07 22:31:50 -0700

    animated sprite

--- Dreamstreet/src/com/dreamstreet/arpg/ui/Minimap.java
+++ Dreamstreet/src/com/dreamstreet/arpg/ui/Minimap.java
@@ -1,7 +1,24 @@
 package com.dreamstreet.arpg.ui;
 
+import java.awt.*;
+
 /**
  * Created on 3/26/2015.
  */
 public class Minimap {
+
+    public Minimap() {
+
+    }
+
+
+    public void tick() {
+
+    }
+
+
+    public void draw(Graphics g) {
+
+    }
+
 }
