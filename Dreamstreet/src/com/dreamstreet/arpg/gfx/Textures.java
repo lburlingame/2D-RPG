38ddf8cf46f4b4a2fc99dd8b1d9b40cc488ef418
lburlingame <lelandburlingame@gmail.com>
2015-05-09 18:14:00 -0700

    Revert "fixed camera zoom jitter"
    This reverts commit 63cef506a82baedf31c648626578dcfd05eedd51.

--- Dreamstreet/src/com/dreamstreet/arpg/gfx/Textures.java
+++ Dreamstreet/src/com/dreamstreet/arpg/gfx/Textures.java
@@ -1,5 +1,7 @@
 package com.dreamstreet.arpg.gfx;
 
+import com.dreamstreet.arpg.Game;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -47,77 +49,77 @@ public class Textures {
         }
     }
 
-    private static BufferedImage getTileGrass() {
+    private static Image getTileGrass() {
         if (TILE_GRASS == null) {
-            TILE_GRASS = grasssheet.getSprite(0,0,64,47);//1
+            TILE_GRASS = Game.toCompatibleImage(grasssheet.getSprite(0,0,64,47));//1
         }
         return TILE_GRASS;
     }
 
-    private static BufferedImage getTileStone() {
+    private static Image getTileStone() {
         if (TILE_STONE == null) {
-            TILE_STONE = tilesheet.getSprite(173,0,32,32);//2
+            TILE_STONE = Game.toCompatibleImage(tilesheet.getSprite(173,0,32,32));//2
         }
         return TILE_STONE;
     }
 
-    private static BufferedImage getTileWater() {
+    private static Image getTileWater() {
         if (TILE_WATER == null) {
-            TILE_WATER = watersheet.getSprite(0,0,64,47);//3
+            TILE_WATER = Game.toCompatibleImage(watersheet.getSprite(0,0,64,47));//3
         }
         return TILE_WATER;
     }
 
-    private static BufferedImage getTileDeadGrass() {
+    private static Image getTileDeadGrass() {
         if (TILE_DEAD_GRASS == null) {
-            TILE_DEAD_GRASS = tilesheet.getSprite(107,33,32,32);//4
+            TILE_DEAD_GRASS = Game.toCompatibleImage(tilesheet.getSprite(107,33,32,32));//4
         }
         return TILE_DEAD_GRASS;
     }
 
-    private static BufferedImage getTileWall() {
+    private static Image getTileWall() {
         if (TILE_WALL == null) {
             TILE_WALL = tilesheet.getSprite(74,132,32,32);//5
         }
         return TILE_WALL;
     }
 
-    private static BufferedImage getIsoTile() {
+    private static Image getIsoTile() {
         if (ISOTILE_GRASS == null) {
-            ISOTILE_GRASS = isosheet.getSprite(0,0,64,47);//6
+            ISOTILE_GRASS = Game.toCompatibleImage(isosheet.getSprite(0,0,64,47));//6
         }
         return ISOTILE_GRASS;
     }
 
-    private static BufferedImage getWhiteTile() {
+    private static Image getWhiteTile() {
         if (ISOTILE_WHITE == null) {
             ISOTILE_WHITE = whitesheet.getSprite(0,0,64,47);//7
         }
         return ISOTILE_WHITE;
     }
 
-    private static BufferedImage getBlackTile() {
+    private static Image getBlackTile() {
         if (ISOTILE_BLACK == null) {
             ISOTILE_BLACK = blacksheet.getSprite(0,0,64,47);//8
         }
         return ISOTILE_BLACK;
     }
 
-    private static BufferedImage getShadowTile() {
+    private static Image getShadowTile() {
         if (ISOTILE_SHADOW == null) {
-            ISOTILE_SHADOW = shadowsheet.getSprite(0,0,64,47);//8
+            ISOTILE_SHADOW = Game.toCompatibleImage(shadowsheet.getSprite(0,0,64,47));//8
         }
         return ISOTILE_SHADOW;
     }
 
-    private static BufferedImage getShadowWaterTile() {
+    private static Image getShadowWaterTile() {
         if (ISOTILE_SHADOWWATER == null) {
-            ISOTILE_SHADOWWATER = shadowwatersheet.getSprite(0,0,64,47);//8
+            ISOTILE_SHADOWWATER = Game.toCompatibleImage(shadowwatersheet.getSprite(0,0,64,47));//8
         }
         return ISOTILE_SHADOWWATER;
     }
 
-    private static BufferedImage getTileDoor() {
+    private static Image getTileDoor() {
         if (TILE_DOOR == null) {
             TILE_DOOR = tilesheet.getSprite(272,33,32,32);//100+
         }
